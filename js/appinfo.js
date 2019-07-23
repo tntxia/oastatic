6139b2c6325ddab89d8087c37a86d01885306070
tntxia <tntxia@163.com>
2019-07-23 14:12:04 +0800

    调换一下位置

--- js/appinfo.js
+++ js/appinfo.js
@@ -5,13 +5,5 @@ $(function() {
         $(".company-name").html(data.companyName);
         $(".company-name-en").html(data.companyNameEn);
     });
-	$.ajax({
-		url:webRoot+"/logininfo.do"
-	}).done(res=>{
-		$("#loginList").empty();
-		$("#online-num").text(res.loginList.length);
-		$.each(res.loginList,function(i,d){
-			$("#loginList").append(d+";")
-		})
-	})
+	
 })
--- js/top.js
+++ js/top.js
@@ -1,5 +1,15 @@
 $(function() {
 
+	$.ajax({
+		url:webRoot+"/logininfo.do"
+	}).done(res=>{
+		$("#loginList").empty();
+		$("#online-num").text(res.loginList.length);
+		$.each(res.loginList,function(i,d){
+			$("#loginList").append(d+";")
+		})
+	})
+
     $.ajax({
         url: webRoot + "/menu!list.do"
     }).done(res => {
