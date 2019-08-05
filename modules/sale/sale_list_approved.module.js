45a6b3deca73357d0f64cdc169201ff951486184
tntxia <tntxia@163.com>
2019-08-05 10:59:47 +0800

    上传

--- modules/sale/sale_list_approved.module.js
+++ modules/sale/sale_list_approved.module.js
@@ -1,47 +1,47 @@
-(function(name,module){
-if(!window.modules){
-window.modules=Object.create(null);
-};
-window.modules[name]=module();
-})('sale_list_approved',function(){
-var module=Object.create(null);
-var exports = Object.create(null);
-module.exports=exports;
-exports.init = function(){
-	
-	let url = webRoot + '/sale/sale!approvedList.do';
-	
-	new Vue({
-		el: '#app',
-		data: {
-			loading: false,
-			dataset: {
-				url: url,
-				method: 'post',
-				pageSize: 50
-			},
-			form: {
-				model : null,
-				coname : null
-			}
-		},
-		mounted() {
-			this.loadData();
-		},
-		methods: {
-			getUrl:function(row){
-				return webRoot
-					+ "/sale/ddgl/detailAudited.mvc?id="+row.id;
-			},
-			loadData() {
-			},
-			query() {
-				let datagrid = this.$refs["datagrid"];
-				datagrid.setParams(this.form);
-				datagrid.loadData();
-			}
-		}
-	});
-	
-};
-return module.exports;});
+(function(name, module) {
+    if (!window.modules) {
+        window.modules = Object.create(null);
+    };
+    window.modules[name] = module();
+})('sale_list_approved', function() {
+    var module = Object.create(null);
+    var exports = Object.create(null);
+    module.exports = exports;
+    exports.init = function() {
+
+        let url = webRoot + '/sale/sale!approvedList.do';
+
+        new Vue({
+            el: '#app',
+            data: {
+                loading: false,
+                dataset: {
+                    url: url,
+                    method: 'post',
+                    pageSize: 50
+                },
+                form: {
+                    epro: null,
+                    coname: null
+                }
+            },
+            mounted() {
+                this.loadData();
+            },
+            methods: {
+                getUrl: function(row) {
+                    return webRoot +
+                        "/sale/ddgl/detailAudited.mvc?id=" + row.id;
+                },
+                loadData() {},
+                query() {
+                    let datagrid = this.$refs["datagrid"];
+                    datagrid.setParams(this.form);
+                    datagrid.loadData();
+                }
+            }
+        });
+
+    };
+    return module.exports;
+});
